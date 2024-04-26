17d43dc070d73e04ade9f7a0f1d715b575c00464
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-04-26 18:03:19 +0300

    updated sum of two nums

--- main.dart
+++ main.dart
@@ -4,6 +4,10 @@ void main() {
   //two number and returns sum
   int sum(int a, int b) {
     return a + b;
-  }
+  };
+  int num1 = 5;
+  int num2 = 3;
+  int result = sum(num1, num2);
+  print("The sum of $num1 and $num2 is $result");
 }
   
